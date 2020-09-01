ee4782e91f8003fc6609302e68421944a278eb0f
Renato Filho <renatosousafilho@gmail.com>
2020-09-01 15:39:33 -0300

    fix desafio4

--- desafio4.sql
+++ desafio4.sql
@@ -2,4 +2,4 @@ CREATE VIEW top_3_artistas AS
 SELECT a.name as artista, count(s.user_id) as seguidores FROM subscriptions AS s
 INNER JOIN artists AS a ON s.artist_id=a.artist_id
 GROUP BY a.artist_id
-ORDER BY seguidores DESC LIMIT 3;
+ORDER BY seguidores DESC, artista ASC LIMIT 3;
