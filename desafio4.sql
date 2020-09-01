9981cacab5ae2adf582918edbe92642fcdc5ef79
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-09-01 20:25:02 -0300

    Arrumando desafio4 com LIMIT

--- desafio4.sql
+++ desafio4.sql
@@ -5,3 +5,4 @@ COUNT(s.art_id) AS seguidores
 FROM SpotifyClone.artistas AS a INNER JOIN SpotifyClone.seguidores AS s ON a.art_id = s.art_id
 GROUP BY artista
 ORDER BY seguidores DESC, artista;
+LIMIT 3;
