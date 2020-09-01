bf58e8dd922097fd365652450825d730a714563e
Renato Filho <renatosousafilho@gmail.com>
2020-09-01 15:32:09 -0300

    fix cc

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS
 SELECT 
-   ROUND(min(p.value),2) as faturamento_minimo,
-   ROUND(max(p.value),2) as faturamento_maximo,
-   ROUND(avg(p.value),2) as faturamento_medio,
-   ROUND(sum(p.value),2) as faturamento_total
+ROUND(min(p.value),2) as faturamento_minimo,
+ROUND(max(p.value),2) as faturamento_maximo,
+ROUND(avg(p.value),2) as faturamento_medio,
+ROUND(sum(p.value),2) as faturamento_total
 FROM plans AS p
 INNER JOIN users AS u ON u.plan_id=p.plan_id;
