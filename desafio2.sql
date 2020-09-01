726dae16aa7cfceff8a67d84bd5f0806c1747f81
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-09-01 17:06:47 -0300

    Arrumando codeclimate desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW estatisticas_musicais as 
 SELECT  
-	(SELECT count(can_id) FROM cancoes) AS cancoes,
-	(SELECT count(art_id) FROM artistas) AS artistas,
-	(SELECT count(alb_id) FROM albuns) AS albuns;
+(SELECT count(can_id) FROM cancoes) AS cancoes,
+(SELECT count(art_id) FROM artistas) AS artistas,
+(SELECT count(alb_id) FROM albuns) AS albuns;
